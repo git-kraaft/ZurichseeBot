a1f8ea5dcf15a81915f2d3e4fe9456508bdd4f99
kraaft <github@zeugs.ch>
2024-06-22 12:36:40 +0200

    moved sensitive data to config.js

---
+++ config_templ.js
@@ -0,0 +1,5 @@
+const TOKEN = 'token_goes_here';
+const INITCHATIDS = [aaaaaaaaa,bbbbbbbbb]; // add ChatIDs of subscribers that you want to add initially after a restart 
+
+exports.TOKEN = TOKEN
+exports.INITCHATIDS = INITCHATIDS
