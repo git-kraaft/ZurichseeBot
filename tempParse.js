f981c77af801cf54d972a91a585af3154173a793
git-kraaft <55582610+git-kraaft@users.noreply.github.com>
2025-01-20 15:36:10 +0100

    Switch to API call

--- tempParse.js
+++ tempParse.js
@@ -1,24 +1,11 @@
-const cheerio = require('cheerio');
-
-// in Safari -> Webinformationen einblenden -> Elemente -> den gewünschten Inhalt identifizieren -> right click -> Kopieren -> Selektorenpfad
-// Google Chrome DevTools (F12) -> select Element in Dev View -> right click ... on the left -> Copy -> Copy Selector
-const cssPathMyth = "body > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(145) > td:nth-child(2)";
-const cssPathTief = "body > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(106) > td:nth-child(2)";
-
 const tempParse = function (url) {
   const fetchPromise = fetch(url);
   return fetchPromise.then(response => {
     return response.text();
-  }).then(html => {
-    const $ = cheerio.load(html);
-    if (html.includes("Mythenquai")) {
-      cssPath = cssPathMyth;
-      //console.log("Myth css " + cssPath);
-    } else {
-      cssPath = cssPathTief;
-      //console.log("Else css " + cssPath);
-    }
-    return $(cssPath).text().trim();
+  }).then(content_json => {
+    // console.log(`JSON: ${content_json}`);
+    const waterTemp = JSON.parse(content_json).result[0].values.water_temperature.value;
+    return waterTemp;
   });
 };
 
