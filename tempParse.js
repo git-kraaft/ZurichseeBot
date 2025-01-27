0a613f81088a2d0100b75978b6fb1bd31875a04c
git-kraaft <55582610+git-kraaft@users.noreply.github.com>
2025-01-27 10:42:39 +0100

    added catch block

--- tempParse.js
+++ tempParse.js
@@ -1,4 +1,4 @@
-const tempParse = function (url) {
+const tempParse = function(url) {
   const fetchPromise = fetch(url);
   return fetchPromise.then(response => {
     return response.text();
@@ -6,6 +6,9 @@ const tempParse = function (url) {
     // console.log(`JSON: ${content_json}`);
     const waterTemp = JSON.parse(content_json).result[0].values.water_temperature.value;
     return waterTemp;
+  }).catch(err => {
+    console.error(`Error in tempParse: ${err.message}`);
+    return null;
   });
 };
 
